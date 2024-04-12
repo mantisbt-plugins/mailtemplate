b2fc3dd27657a50cb8794dc79cf2d9d3d07990ad
Cas <cas@nuy.info>
2024-04-12 09:41:12 +0200

    Update template_api.php
    Added 4 exxtra fields for the note template ( bug_id, bug_url, summary & category)

--- core/template_api.php
+++ core/template_api.php
@@ -175,6 +175,18 @@ function email_template_bugnote( $p_bugnote, $p_project_id, $p_show_time_trackin
 	$note = $p_bugnote->note;
 	$variables['note'] = $note;
 
+	// fetch additional data
+	$t_bug_id =  $p_bugnote->bug_id;
+	$variable['bug_id'] = $t_bug_id;
+	$variable['bug_url'] = string_get_bug_view_url_with_fqdn( $t_bug_id );
+	$t_query 	= "SELECT summary,name FROM {bug} B , {category}  WHERE B.category_id = C.id and B.id = $t_bug_id";
+	$t_result	= db_query($t_query);
+	$row_1 = db_fetch_array($t_result))
+	$t_summary	= $row_1['summary'];
+	$t_category	= $t_row['name'];
+	$variable['summary'] = $t_summary;
+	$variable['category'] = $t_category;
+	
 	// Load the template
 	$template_definition = config_get( 'note_mailtemplate' );
 	$template = file_get_contents($template_definition);
