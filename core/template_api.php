a88de9e9430f5cefe5694dd5841a443950925530
pkbarbiedoll <adrya.stembridge@gmail.com>
2024-04-12 09:26:02 -0400

    Update template_api.php
    Fixed type-o in line which previously caused this error:
    
    "PHP Parse error: Unclosed '{' on line 143 does not match ')' in template_api.php on line 184 #2 "

--- core/template_api.php
+++ core/template_api.php
@@ -181,7 +181,7 @@ function email_template_bugnote( $p_bugnote, $p_project_id, $p_show_time_trackin
 	$variable['bug_url'] = string_get_bug_view_url_with_fqdn( $t_bug_id );
 	$t_query 	= "SELECT summary,name FROM {bug} B , {category}  WHERE B.category_id = C.id and B.id = $t_bug_id";
 	$t_result	= db_query($t_query);
-	$row_1 = db_fetch_array($t_result))
+	$row_1 = db_fetch_array($t_result);
 	$t_summary	= $row_1['summary'];
 	$t_category	= $t_row['name'];
 	$variable['summary'] = $t_summary;
